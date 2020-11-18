041432ab7f58ce35a5b6dfe249c554af3bfefb94
Gin757306533 <757306533@qq.com>
2020-11-18 23:47:27 +0800

    add test override rule for eslint

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    const expectedResult = { name: "jiexu" };
+    const mockResult = { data: expectedResult };
+    axios.get.mockResolvedValue(mockResult);
+    await expect(getUsers()).resolves.toEqual(expectedResult);
   });
 });
